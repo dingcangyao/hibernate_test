c1125adbfc651b92d8e1cc9b4f64d913c264209c
faker <faker@163.com>
2022-08-22 16:09:29 +0800

    hibernate 一对多关系建立，测试  customer 和 orders

--- src/hibernate.md
+++ src/hibernate.md
@@ -14,5 +14,38 @@
 - 使用Hibernate API 完成操作
 - 如果你要读取 src 中的  xml文件，就需要在pom文件中配置 resources   
 
-## Hibernatenate级联操作
+<font color="gree">## Hibernatenate级联操作</font>
+1. 一对多关系  
+    客户和订单：每个客户可以购买多个产品，生成多个订单，但是每个订单只能属于一个客户  
+    数据库中 通过主键外键体现。 java 中就是通过集合关联来实现
+2. 多对多关系  
+    学生选课：一门课程可以被多个学生选择，一个学生可以选择多个课程，都是多。  
+    数据库中是通过 两个一对一来对应，创建一个中间表，来关联 学生和课程表。java 其实还是通过集合来进行映射。  
+
+Java 和数据库对于这两种关系的体现完全是两种不同的方式，hibernate 就是负责两者之间的转换
+
+customer  
+
+        <!-- 一对多的映射  customer 是1 order是多  -->
+        <set name="orders" table="orders">
+            <key column="cid"></key>
+            <one-to-many class="com.yaoge.entity.Orders"></one-to-many>
+        </set>  
+- set 标签来配置实体类中的集合属性 orders
+- name 一的一方的实体类属性名
+- table 多的一方的表 名
+- key 外键
+- one-to-many 与集合泛型的实体类对应    
+     
+   orders
+
+        <!-- 多对一的映射  customer 是1 order是多  -->
+        <many-to-one name="customer" class="com.yaoge.entity.Customer" column="cid"></many-to-one>  
+- many-to-one 配置实体类对应的对象属性
+- name 属性名
+- class 属性对应的类
+- column 对应一的外键   
+
+
+
 
---
+++ src/main/java/com/yaoge/test/Test2.java
@@ -0,0 +1,42 @@
+package com.yaoge.test;
+
+import com.yaoge.entity.Customer;
+import com.yaoge.entity.Orders;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import javax.persistence.criteria.Order;
+
+/**
+ * create by yaoge
+ * 2022/8/22 15:57
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        Configuration configure = new Configuration().configure();
+        SessionFactory sessionFactory = configure.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        //开启事务
+        Transaction transaction = session.beginTransaction();
+
+        //创建 Customer
+        Customer customer = new Customer();
+        customer.setName("张三");
+        //创建Orders
+        Orders orders = new Orders();
+        orders.setName("订单1");
+
+        //建立关联关系
+        orders.setCustomer(customer);
+        //保存
+        session.save(customer);
+        session.save(orders);
+        customer.setName("李四");//持久态的对象更改属性会在提交事务之后保存到数据库
+        //提交事务
+        transaction.commit();
+        //关闭session
+        session.close();
+    }
+}
