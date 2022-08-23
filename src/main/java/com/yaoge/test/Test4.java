f8afc1c44014e5f8f0f3cdf750e42fc468ad50e1
faker <faker@163.com>
2022-08-23 15:52:35 +0800

    hibernate HQL 语句演示

--- src/main/java/com/yaoge/test/Test4.java
+++ src/main/java/com/yaoge/test/Test4.java
@@ -1,18 +1,19 @@
 package com.yaoge.test;
 
-import com.yaoge.entity.Account;
-import com.yaoge.entity.Course;
-import com.yaoge.entity.Customer;
-import com.yaoge.entity.Orders;
+import com.yaoge.entity.*;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 
 /**
  * create by yaoge
@@ -54,6 +55,153 @@ public class Test4 {
         Orders orders = session.get(Orders.class, 1);
 //        System.out.println(orders.getCustomer().prin());
         orders.getCustomer().prin();
+    }
+
+    /**
+     * 插入数据
+     */
+    @Test
+    public void testI() {
+
+        People people = new People();
+        people.setName("张双江");
+        people.setMoney(4563.0);
+        session.save(people);
+    }
+
+
+    /**
+     * 测试HQL   查询 People 表中全部数据
+     */
+    @Test
+    public void testHQL1() {
+
+        String hql = "from People";//这里一定要写类名，可以不写全类名
+        Query query = session.createQuery(hql);
+        List list = query.list();
+        list.stream().forEach((item) -> System.out.println(item));
+
+
+    }
+
+    /**
+     * 测试HQL   分页查询
+     */
+    @Test
+    public void testHQLPage() {
+
+        String hql = "from People";//这里一定要写类名，可以不写全类名
+        Query query = session.createQuery(hql);
+        query.setFirstResult(3);//设置起始查询记录，就是从第几个查询
+        query.setMaxResults(3);//每次查询几个
+
+        List list = query.list();
+        list.stream().forEach((item) -> System.out.println(item));
+
+
+    }
+
+    /**
+     * 测试HQL   where 查询
+     * 使用方式和 SQL 没有区别
+     */
+    @Test
+    public void testHQLWhere() {
+
+        String hql = "from People where id=3";//这里一定要写类名，可以不写全类名
+        Query query = session.createQuery(hql);
+        List list = query.list();//返回一个集合
+        //        query.uniqueResultOptional()
+        //        query.uniqueResult()
+        //如果只是查询单条记录，可以使用上面两个方法，返回为空
+
+        list.stream().forEach((item) -> System.out.println(item));
+
+
+    }
+    /**
+     * 测试HQL  模糊查询
+     */
+    @Test
+    public void testHQLLike() {
+
+        String hql = "from People where name like '%双%'";//这里一定要写类名，可以不写全类名
+        Query query = session.createQuery(hql);
+        List list = query.list();//返回一个集合
+        //        query.uniqueResultOptional()
+        //        query.uniqueResult()
+        //如果只是查询单条记录，可以使用上面两个方法，返回为空
+
+        list.stream().forEach((item) -> System.out.println(item));
+
+
+    }
+    /**
+     * 测试HQL  Order By 排序
+     * 也是跟SQL是一样的使用
+     */
+    @Test
+    public void testHQLOrder() {
+
+        String hql = "from People order by money desc ";
+        //desc 是倒序， asc（默认） 是正序
+        Query query = session.createQuery(hql);
+        List list = query.list();//返回一个集合
+
+        list.stream().forEach((item) -> System.out.println(item));
+
+
+    }
+   /**
+     * 测试HQL
+    * 查询单一属性
+    * 需要用到
+     */
+    @Test
+    public void testHQLAttribute() {
+
+        String hql = "select name from People where id=4 ";
+        Query query = session.createQuery(hql);
+//        List<String> list = query.list();  //查询多个记录的单属性，就是直接用list接收就行了
+//        System.out.println(list);
+        String name = (String) query.uniqueResult();
+        // query.uniqueResult 返回值就只能是单个对象，如果结果有多个，就会报错
+//        List<People> list = query.list();//其实不指定类型，默认的转成的就是Object数组
+        System.out.println(name);
+    }
+  /**
+     * 测试HQL
+   * 测试占位符
+     */
+    @Test
+    public void testHQLZhanwei() {
+
+        String hql = " from People where name=:name";
+        Query query = session.createQuery(hql);
+        query.setString("name","张三");
+        List<People> list = query.list();
+        System.out.println(list);
+    }
+  /**
+     * 测试HQL
+   * 级联查询, 就是参数可以直接设置为实体
+     */
+    @Test
+    public void testHQLCascade() {
+
+        String hql1 = " from Customer where name=:name";
+        Query query1 = session.createQuery(hql1);
+        query1.setString("name","李四");
+        List<Customer> list = query1.list();
+        System.out.println("用户："+list);
+        list.forEach(element->{
+            String hql2 = " from Customer where name=:name";
+            Query query2 = session.createQuery(hql2);
+            query2.setString("name","李四");
+            List<Customer> list2 = query2.list();
+            System.out.println("对应的订单");
+            System.out.println(list2);
+        });
     }
 
 
