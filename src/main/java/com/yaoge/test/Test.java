a6e80ae62291073090c3efe4cb33816d0339ff3e
faker <faker@163.com>
2022-08-26 15:13:13 +0800

    测试了一些东西，hibernate的获取SessionFactory的方式有好几种

--- src/main/java/com/yaoge/test/Test.java
+++ src/main/java/com/yaoge/test/Test.java
@@ -18,10 +18,18 @@ public class Test {
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         //获取Session
         Session session = sessionFactory.openSession();
-        People people = new People();
-        people.setName("李四");
-        people.setMoney(2000.0);
-        session.save(people);
+        People people = new People();//刚new 出来没有设置OID 就是临时状态
+//        people.setName("王阳明");
+//        people.setMoney(2000.0);
+//        session.save(people);//保存之后变成持久态
+//        System.out.println(people);
+//        session.evict(people);//把指定对象的缓存清除，变成游离态，因为此时的对象有ID，但是建立关系
+//        people.setName("王四重");
+//        session.update(people);//应该又转换成持久态
+
+        people.setName("haha");
+        people.setId(5);
+        session.delete(people);
         session.beginTransaction().commit();
         session.close();
 
